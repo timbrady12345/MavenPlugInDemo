165c48b0e2b53d29b11167270b4876da818eb776
Tim Brady <70961965+timbrady12345@users.noreply.github.com>
2023-03-16 11:19:54 -0400

    whitespace2

--- src/main/java/com/hcl/pluginExample/App.java
+++ src/main/java/com/hcl/pluginExample/App.java
@@ -9,14 +9,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        ArrayList<Integer> nums = new ArrayList<Integer>();
-        nums.add(5);
+        ArrayList<Integer> nums = new ArrayList<Integer>();          
+        nums.add(5);              
 //        nums.forEach( (n) -> {System.out.println(n); } );
 //        String multilineString = "multilineString helps \n \n developers \n explore Java.";
 //        List<String> lines = multilineString.lines()
-//          .filter(line -> !line.isBlank())
-//          .map(String::strip)
-//          .collect(Collectors.toList());
+//          .filter(line -> !line.isBlank())         
+//          .map(String::strip)          
+//          .collect(Collectors.toList());         
     }
 }
                                
