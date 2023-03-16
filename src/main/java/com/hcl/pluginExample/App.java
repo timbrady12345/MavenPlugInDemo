d23b20bae1751286b2607fbe8798e85a5c9de2e4
Tim Brady <70961965+timbrady12345@users.noreply.github.com>
2023-03-16 10:16:43 -0400

    Initial Commit

---
+++ src/main/java/com/hcl/pluginExample/App.java
@@ -0,0 +1,21 @@
+package com.hcl.pluginExample;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(5);
+//        nums.forEach( (n) -> {System.out.println(n); } );
+//        String multilineString = "multilineString helps \n \n developers \n explore Java.";
+//        List<String> lines = multilineString.lines()
+//          .filter(line -> !line.isBlank())
+//          .map(String::strip)
+//          .collect(Collectors.toList());
+    }
+}
