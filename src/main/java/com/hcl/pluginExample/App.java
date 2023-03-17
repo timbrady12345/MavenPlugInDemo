6631eada1503e7e630e4c03aadd2e535219b35b3
Tim Brady <70961965+timbrady12345@users.noreply.github.com>
2023-03-17 00:12:28 -0400

    trailing whitespace

--- src/main/java/com/hcl/pluginExample/App.java
+++ src/main/java/com/hcl/pluginExample/App.java
@@ -6,16 +6,16 @@ import java.util.stream.Collectors;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args )                  
     {
-        System.out.println( "Hello World!" );              
-        ArrayList<Integer> nums = new ArrayList<Integer>();          
+        System.out.println( "Hello World!" );                            
+        ArrayList<Integer> nums = new ArrayList<Integer>();                    
         nums.add(5);              
 //        nums.forEach( (n) -> {System.out.println(n); } );
 //        String multilineString = "multilineString helps \n \n developers \n explore Java.";
 //        List<String> lines = multilineString.lines()
 //          .filter(line -> !line.isBlank())         
-//          .map(String::strip)          
+//          .map(String::strip)         
 //          .collect(Collectors.toList());         
     }
 }
