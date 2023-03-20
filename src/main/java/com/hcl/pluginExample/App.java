32b25e4b97f7529999b4b30794b1715bfcdab1f8
Tim Brady <70961965+timbrady12345@users.noreply.github.com>
2023-03-20 11:00:38 -0400

    test1

--- src/main/java/com/hcl/pluginExample/App.java
+++ src/main/java/com/hcl/pluginExample/App.java
@@ -6,17 +6,11 @@ import java.util.stream.Collectors;
 
 public class App 
 {
-    public static void main( String[] args )                  
+    public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );                            
-        ArrayList<Integer> nums = new ArrayList<Integer>();                    
-        nums.add(5);              
-//        nums.forEach( (n) -> {System.out.println(n); } );
-//        String multilineString = "multilineString helps \n \n developers \n explore Java.";
-//        List<String> lines = multilineString.lines()
-//          .filter(line -> !line.isBlank())         
-//          .map(String::strip)         
-//          .collect(Collectors.toList());         
+        System.out.println( "Hello World!" );
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(5);
     }
 }
                                
