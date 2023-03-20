39de5e898b86049abc0b113f470e237a905686a2
Tim Brady <70961965+timbrady12345@users.noreply.github.com>
2023-03-20 11:16:22 -0400

    quote change

--- src/main/java/com/hcl/pluginExample/App.java
+++ src/main/java/com/hcl/pluginExample/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String str = "Double Quote Test";
         ArrayList<Integer> nums = new ArrayList<Integer>();
         nums.add(5);
     }
