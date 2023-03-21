9633b7d0bd560f481e648202d902ee43cf079825
Tim Brady <70961965+timbrady12345@users.noreply.github.com>
2023-03-21 17:40:36 -0400

    DO NOT SUBMIT forbidden test

--- src/main/java/com/hcl/pluginExample/App.java
+++ src/main/java/com/hcl/pluginExample/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        String str = " DO NOT SUBMIT ";
+        String str = "DO NOT SUBMIT";
         ArrayList<Integer> nums = new ArrayList<Integer>();
         nums.add(5);
     }
