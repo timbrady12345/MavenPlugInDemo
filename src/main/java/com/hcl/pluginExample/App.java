e8c50564578004113e665a9fea60a768961f72ed
Tim Brady <70961965+timbrady12345@users.noreply.github.com>
2023-03-21 22:43:37 -0400

    testing pre-commit 3

--- src/main/java/com/hcl/pluginExample/App.java
+++ src/main/java/com/hcl/pluginExample/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        String str = "Testing a string again";
+        String str = "Testing a string";
         ArrayList<Integer> nums = new ArrayList<Integer>();
         nums.add(5);
     }
